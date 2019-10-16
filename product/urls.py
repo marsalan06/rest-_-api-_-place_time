3b0b518d2dd4cc48a034df2d5e7ba6855ca1ae8d
marsalan06 <arsalan.9798@gmail.com>
2019-10-16 18:30:02 +0500

    """contains PUT request"""

--- product/urls.py
+++ product/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from .import views
 urlpatterns = [
     path('products/',views.Product_list),
+    path('products/<int:pk>/',views.product_detail),
 ]
--- product/views.py
+++ product/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from .models import Product
 from .serializer import Productserializer
 from rest_framework import status
+from django.core.exceptions import ObjectDoesNotExist
 # Create your views here.
 @api_view(['GET','POST'])
 def Product_list(request):
@@ -16,3 +17,21 @@ def Product_list(request):
             serializer.save()
             return Response(serializer.data, status= status.HTTP_201_CREATED)
         return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET','PUT'])       
+def product_detail(request,pk):
+    try:
+        obj= Product.objects.get(id= pk)
+    except ObjectDoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method=='GET': #CREATED CONDITION
+        serializer=Productserializer(obj)
+        return Response(serializer.data)
+    elif request.method=='PUT': #CREATED CONDITION
+        serializer=Productserializer(obj,data=request.data) #REQUIRES OBJECT 
+  # AND THE CHANGE 
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status= status.HTTP_400_BAD_REQUEST)
+
